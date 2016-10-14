4f53b15d5e951672b452787b3ecc1d1aa7e5cd72
李吉元 <819491243@qq.com>
2016-10-14 15:27:09 +0800

    修改Manfiests，创建splashActivity,闪屏

--- app/src/main/java/cn/ucai/fullcenter/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fullcenter/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package cn.ucai.fullcenter;
+package cn.ucai.fullcenter.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import cn.ucai.fullcenter.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/cn/ucai/fullcenter/activity/SpflashActivity.java
@@ -0,0 +1,37 @@
+package cn.ucai.fullcenter.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.ucai.fullcenter.R;
+
+
+public class SpflashActivity extends AppCompatActivity {
+    static final long SLEEP_TIME=2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spflash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long start = System.currentTimeMillis();
+                long cost_time=System.currentTimeMillis()-start;
+                if(SLEEP_TIME-cost_time>0){
+                    try {
+                        Thread.sleep(SLEEP_TIME-cost_time);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                startActivity(new Intent(SpflashActivity.this,MainActivity.class));
+            }
+        }).start();
+    }
+}
