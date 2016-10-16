f9629db185bb715a4ab6bd79e457611b432ce1e3
李吉元 <819491243@qq.com>
2016-10-16 21:35:01 +0800

    创建Utils包 导入L，MFGT文件

--- app/src/main/java/cn/ucai/fullcenter/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fullcenter/activity/MainActivity.java
@@ -2,8 +2,10 @@ package cn.ucai.fullcenter.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import cn.ucai.fullcenter.R;
+import cn.ucai.fullcenter.utils.L;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        L.i("MainActivity onCreate");
     }
+
+    public void onCheckedChange(View v){}
 }
