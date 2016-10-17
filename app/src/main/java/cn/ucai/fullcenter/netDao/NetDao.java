5c04abf8dcb8dc8d9bd3d793c821762d1749492b
李吉元 <819491243@qq.com>
2016-10-17 20:20:16 +0800

    添加网络请求工具类和新品网络请求的方法

---
+++ app/src/main/java/cn/ucai/fullcenter/netDao/NetDao.java
@@ -0,0 +1,23 @@
+package cn.ucai.fullcenter.netDao;
+
+import android.content.Context;
+
+import cn.ucai.fullcenter.I;
+import cn.ucai.fullcenter.bean.NewGoodsBean;
+
+/**
+ * Created by Administrator on 2016/10/17.
+ */
+public class NetDao {
+
+    public static void downLoadNewGoods(Context mcontext, int pageId, OkHttpUtils.OnCompleteListener<NewGoodsBean> listener){
+        OkHttpUtils utils=new OkHttpUtils(mcontext);
+        utils.setRequestUrl(I.REQUEST_FIND_NEW_BOUTIQUE_GOODS)
+                .addParam(I.NewAndBoutiqueGoods.CAT_ID,String.valueOf(I.CAT_ID))
+                .addParam(I.PAGE_ID,String.valueOf(pageId))
+                .addParam(I.PAGE_SIZE,String.valueOf(I.PAGE_SIZE))
+                .targetClass(NewGoodsBean[].class)
+                .execute(listener);
+    }
+
+}
