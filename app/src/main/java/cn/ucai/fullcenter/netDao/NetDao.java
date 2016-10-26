6a418986348b0655f7505f301ac0907083dd3570
李吉元 <819491243@qq.com>
2016-10-26 20:44:10 +0800

    更新NetDao

--- app/src/main/java/cn/ucai/fullcenter/netDao/NetDao.java
+++ app/src/main/java/cn/ucai/fullcenter/netDao/NetDao.java
@@ -166,4 +166,13 @@ public class NetDao {
                 .execute(listener);
     }
 
+    public static void addCollectGoods(Context context,String username,int goodId, OkHttpUtils.OnCompleteListener<MessageBean> listener){
+        OkHttpUtils<MessageBean> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_ADD_COLLECT)
+                .addParam(I.Collect.USER_NAME,username)
+                .addParam(I.Collect.GOODS_ID,String.valueOf(goodId))
+                .targetClass(MessageBean.class)
+                .execute(listener);
+    }
+
 }
