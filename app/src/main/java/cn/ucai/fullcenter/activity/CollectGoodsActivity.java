85c93db61b58a40b8c048fe9016897b9f44f05af
李吉元 <819491243@qq.com>
2016-10-26 16:44:41 +0800

    创建收藏商品二级界面布局文件

---
+++ app/src/main/java/cn/ucai/fullcenter/activity/CollectGoodsActivity.java
@@ -0,0 +1,165 @@
+package cn.ucai.fullcenter.activity;
+
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.ucai.fullcenter.FuLiCenterApplication;
+import cn.ucai.fullcenter.I;
+import cn.ucai.fullcenter.R;
+import cn.ucai.fullcenter.adapter.CollectGoodsAdapter;
+import cn.ucai.fullcenter.adapter.GoodsAdapter;
+import cn.ucai.fullcenter.bean.CollectBean;
+import cn.ucai.fullcenter.bean.NewGoodsBean;
+import cn.ucai.fullcenter.bean.User;
+import cn.ucai.fullcenter.netDao.NetDao;
+import cn.ucai.fullcenter.netDao.OkHttpUtils;
+import cn.ucai.fullcenter.utils.CommonUtils;
+import cn.ucai.fullcenter.utils.ConvertUtils;
+import cn.ucai.fullcenter.utils.L;
+import cn.ucai.fullcenter.views.DisplayUtils;
+import cn.ucai.fullcenter.views.SpaceItemDecoration;
+
+public class CollectGoodsActivity extends BaseActivity {
+    @BindView(R.id.tv_refresh)
+    TextView tvRefresh;
+    @BindView(R.id.rlv)
+    RecyclerView rlv;
+    @BindView(R.id.sfl)
+    SwipeRefreshLayout sfl;
+
+    CollectGoodsActivity mContext;
+    CollectGoodsAdapter mAdapter;
+    ArrayList<CollectBean> mList;
+    int pageId = 1;
+    LinearLayoutManager mLinearLayoutManager;
+
+    User user;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_collect_goods);
+        ButterKnife.bind(this);
+        mContext = this;
+        mList = new ArrayList<>();
+        mAdapter = new CollectGoodsAdapter(mList, mContext);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void initView() {
+        DisplayUtils.initBackWithTitle(mContext, getResources().getString(R.string.collect_goods_title));
+
+        sfl.setColorSchemeColors(getResources().getColor(R.color.google_blue),
+                getResources().getColor(R.color.google_yellow),
+                getResources().getColor(R.color.google_red),
+                getResources().getColor(R.color.google_green));
+        mLinearLayoutManager = new GridLayoutManager(mContext, I.COLUM_NUM);
+        rlv.setLayoutManager(mLinearLayoutManager);
+        rlv.setHasFixedSize(true);
+        rlv.setAdapter(mAdapter);
+        rlv.addItemDecoration(new SpaceItemDecoration(16));
+    }
+
+    /**
+     * 创建一个监听器
+     */
+    @Override
+    protected void setListener() {
+        setPullUpListener();
+        setPullDownListener();
+    }
+
+    /**
+     * 下拉刷新
+     */
+    private void setPullDownListener() {
+        sfl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                sfl.setRefreshing(true);
+                tvRefresh.setVisibility(View.VISIBLE);
+                pageId = 1;
+                downLoadColletGoods(I.ACTION_PULL_DOWN);
+            }
+        });
+    }
+
+    /**
+     * 上拉刷新
+     */
+    private void setPullUpListener() {
+        rlv.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                int lastPosition = mLinearLayoutManager.findLastVisibleItemPosition();
+                if (newState == RecyclerView.SCROLL_STATE_IDLE
+                        && lastPosition == mAdapter.getItemCount() - 1
+                        && mAdapter.isMore()) {
+                    pageId++;
+                    downLoadColletGoods(I.ACTION_PULL_UP);
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                int firstPosition = mLinearLayoutManager.findFirstVisibleItemPosition();
+                sfl.setEnabled(firstPosition == 0);
+            }
+        });
+    }
+
+    @Override
+    protected void initData() {
+        user = FuLiCenterApplication.getUser();
+        if(user==null){
+            finish();
+        }
+        downLoadColletGoods(I.ACTION_DOWNLOAD);
+    }
+
+    private void downLoadColletGoods(final int action) {
+        NetDao.downloadCollectGoods(mContext, user.getMuserName(), pageId, new OkHttpUtils.OnCompleteListener<CollectBean[]>() {
+            @Override
+            public void onSuccess(CollectBean[] result) {
+                sfl.setRefreshing(false);
+                tvRefresh.setVisibility(View.GONE);
+                mAdapter.setMore(true);
+                if (result != null && result.length > 0) {
+                    ArrayList<CollectBean> list = ConvertUtils.array2List(result);
+                    if (action == I.ACTION_DOWNLOAD || action == I.ACTION_PULL_DOWN) {
+                        mAdapter.initData(list);
+                    } else {
+                        mAdapter.AddData(list);
+                    }
+
+                    if (list.size() < I.PAGE_SIZE_DEFAULT) {
+                        mAdapter.setMore(false);
+                    }
+                } else {
+                    mAdapter.setMore(false);
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                sfl.setRefreshing(false);
+                tvRefresh.setVisibility(View.GONE);
+                CommonUtils.showLongToast(error);
+                L.e("error: " + error);
+
+            }
+        });
+    }
+
+}
